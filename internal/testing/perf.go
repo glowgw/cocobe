e16eb6e21941c2a367d88a1a4ce2004e86fc6253
glowgw <glow@greenworm.io>
2023-07-11 18:41:00 +0700

    Handle multi user call sendTx

--- internal/testing/perf.go
+++ internal/testing/perf.go
@@ -6,7 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	types2 "github.com/cosmos/cosmos-sdk/x/auth/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/dymensionxyz/rollapp/app"
 	"github.com/dymensionxyz/rollapp/app/params"
 	"github.com/dymensionxyz/rollapp/utils"
@@ -27,6 +27,7 @@ type perfClient struct {
 	logger *slog.Logger
 
 	ec params.EncodingConfig
+	kr keyring.Keyring
 
 	baseCtx client.Context
 	txf     tx.Factory
@@ -37,14 +38,18 @@ const (
 )
 
 var (
-	username = "rol-user3"
-	gas      = uint64(1_000_000)
-	gasAdj   = 1.15
+	gas    = uint64(1_000_000)
+	gasAdj = 1.15
 )
 
 func newPerfClient() (*perfClient, error) {
 	logger := slog.Default()
 	cfg := config.Default()
+
+	nodeClient, err := client.NewClientFromNode(cfg.RpcUrl)
+	if err != nil {
+		return nil, err
+	}
 
 	ec := app.MakeEncodingConfig()
 
@@ -53,42 +58,23 @@ func newPerfClient() (*perfClient, error) {
 		WithTxConfig(ec.TxConfig).
 		WithCodec(ec.Codec).
 		WithHomeDir(app.DefaultNodeHome).
-		WithAccountRetriever(types2.AccountRetriever{}).
-		WithInterfaceRegistry(ec.InterfaceRegistry)
-	nodeClient, err := client.NewClientFromNode(cfg.RpcUrl)
-	if err != nil {
-		return nil, err
-	}
-	baseCtx = baseCtx.WithClient(nodeClient)
-	baseCtx = baseCtx.WithFromName(username)
+		WithAccountRetriever(authtypes.AccountRetriever{}).
+		WithInterfaceRegistry(ec.InterfaceRegistry).
+		WithClient(nodeClient)
+
 	kr, err := keyring.New(sdk.KeyringServiceName(), "test", baseCtx.HomeDir, baseCtx.Input, baseCtx.Codec, baseCtx.KeyringOptions...)
 	if err != nil {
 		logger.Error("new keyring error", "err", err)
 		return nil, err
 	}
 
-	record, err := kr.Key(baseCtx.GetFromName())
-	if err != nil {
-		logger.Error("err get key by from name", "err", err)
-		return nil, err
-	}
-	addr, err := record.GetAddress()
-	if err != nil {
-		return nil, err
-	}
-	logger.Info("addr by from name", "addr", addr, "from name", baseCtx.FromName)
-	baseCtx = baseCtx.WithFromAddress(addr)
-	txf := tx.Factory{}.WithChainID(chainID).WithTxConfig(ec.TxConfig).WithKeybase(kr).WithGas(gas).WithGasAdjustment(gasAdj).WithAccountRetriever(types2.AccountRetriever{})
-
-	fromAddr := baseCtx.GetFromAddress()
-	logger.Info("fromAddr", "val", fromAddr)
-
-	accNum, _, err := txf.AccountRetriever().GetAccountNumberSequence(baseCtx, fromAddr)
-	if err != nil {
-		logger.Error("account retriever failed", "err", err)
-		return nil, err
-	}
-	txf = txf.WithAccountNumber(accNum)
+	txf := tx.Factory{}.
+		WithChainID(chainID).
+		WithTxConfig(ec.TxConfig).
+		WithKeybase(kr).
+		WithGas(gas).
+		WithGasAdjustment(gasAdj).
+		WithAccountRetriever(authtypes.AccountRetriever{})
 
 	return &perfClient{
 		ec:      ec,
@@ -96,15 +82,54 @@ func newPerfClient() (*perfClient, error) {
 		baseCtx: baseCtx,
 		txf:     txf,
 		cfg:     cfg,
+		kr:      kr,
 	}, nil
 }
 
-func (pc *perfClient) sendTx() error {
+func (pc *perfClient) GetAccountNumberSequence(name string) (uint64, error) {
+	addr, err := pc.GetAddress(name)
+	if err != nil {
+		return 0, err
+	}
+	_, accSeq, err := pc.txf.AccountRetriever().GetAccountNumberSequence(pc.baseCtx, addr)
+	return accSeq, err
+}
+
+func (pc *perfClient) GetAddress(name string) (sdk.AccAddress, error) {
+	pc.baseCtx = pc.baseCtx.WithFromName(name)
+	record, err := pc.kr.Key(pc.baseCtx.GetFromName())
+	if err != nil {
+		pc.logger.Error("err get key by from name", "err", err)
+		return nil, err
+	}
+	addr, err := record.GetAddress()
+	if err != nil {
+		return nil, err
+	}
+	return addr, nil
+}
+
+func (pc *perfClient) sendTx(user string, accSeq uint64) (uint64, error) {
+	addr, err := pc.GetAddress(user)
+	if err != nil {
+		return 0, err
+	}
+	pc.baseCtx = pc.baseCtx.WithFromAddress(addr)
+
+	fromAddr := pc.baseCtx.GetFromAddress()
+
+	accNum, _, err := pc.txf.AccountRetriever().GetAccountNumberSequence(pc.baseCtx, fromAddr)
+	if err != nil {
+		pc.logger.Error("account retriever failed", "err", err)
+		return 0, err
+	}
+	pc.txf = pc.txf.WithAccountNumber(accNum).WithSequence(accSeq)
+
 	argOption := "over"
 	numberBetting := uint32(40)
 	coin, err := sdk.ParseCoinNormalized("200urax")
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	msg := types.NewMsgDiceBetting(
@@ -115,33 +140,36 @@ func (pc *perfClient) sendTx() error {
 	)
 	if err := msg.ValidateBasic(); err != nil {
 		pc.logger.Error("validate basic failed", "err", err)
-		return err
+		return 0, err
 	}
 
 	pc.baseCtx.BroadcastMode = flags.BroadcastSync
 
 	txUnsign, err := pc.txf.BuildUnsignedTx(msg)
 	if err != nil {
-		return err
+		return 0, err
 	}
-	err = tx.Sign(pc.txf, username, txUnsign, true)
+	err = tx.Sign(pc.txf, user, txUnsign, true)
 	if err != nil {
 		pc.logger.Error("err sign: ", "err", err)
-		return err
+		return 0, err
 	}
 
 	txBytes, err := pc.baseCtx.TxConfig.TxEncoder()(txUnsign.GetTx())
 	if err != nil {
 		pc.logger.Error("encode tx error: ", "err", err)
-		return err
+		return 0, err
 	}
 
 	res, err := pc.baseCtx.BroadcastTx(txBytes)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	pc.logger.Info("broadcast tx: ", "code", res.Code, "tx_hash", res.TxHash)
+	if res.Code == 0 {
+		return accSeq + 1, nil
+	}
 
-	return nil
+	return accSeq, nil
 }
--- internal/testing/perf_test.go
+++ internal/testing/perf_test.go
@@ -1,17 +1,42 @@
 package testing
 
 import (
+	"fmt"
 	"testing"
 )
+
+var listUsers []string
+
+func init() {
+	for i := 1; i < 8; i++ {
+		listUsers = append(listUsers, fmt.Sprintf("rol-user%d", i))
+	}
+}
 
 func TestBet(t *testing.T) {
 	c, err := newPerfClient()
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = c.sendTx()
-	if err != nil {
-		t.Fatal(err)
+	iter := 0
+	for {
+		if iter > 1_000_000 {
+			break
+		}
+
+		var seq uint64
+		firstSeq, err := c.GetAccountNumberSequence(listUsers[0])
+		if err != nil {
+			t.Fatal(err)
+		}
+		seq = firstSeq
+		accSeq, err := c.sendTx(listUsers[0], seq)
+		if err != nil {
+			t.Fatal(err)
+		}
+		seq = accSeq
+
+		iter += 1
 	}
 
 }
